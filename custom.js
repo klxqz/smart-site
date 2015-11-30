6e9b893d431480e129ba44f2e788f9b82d6e227c
korob <korob@192.168.1.180>
2015-11-30 09:28:01 +0500

    разработка 30.11.2015 9:27

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 function initFormControl(selector) {
-    selector.find('input[type=text],input[type=password],select,textarea').addClass('form-control');
+    selector.find('input[type=text],input[type=email],input[type=password],select,textarea').addClass('form-control');
     selector.find('input[type=submit]').addClass('btn btn-primary');
 }
 
