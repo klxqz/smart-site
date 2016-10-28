32912570b9ccbdc16daad9a95584117615d1b378
korob <korob@192.168.1.180>
2016-10-28 09:18:25 +0500

    перед изменением отложенной загрузки js

--- !js/delayed-loading.js
+++ !js/delayed-loading.js
@@ -1,31 +1,17 @@
 function delayedLoading() {
     function callbackCss(load) {
-        console.log(load.type + ': ' + load.target.href);
+        console.log(load.type + ' css: ' + load.target.href);
     }
     if (window.delayCss.length) {
+        var df = document.createDocumentFragment();
         for (var i in window.delayCss) {
             var link = document.createElement("link");
-            if (link.readyState && !link.onload) {
-                /*IE, Opera*/
-                js.onreadystatechange = function () {
-                    if (link.readyState == "loaded" || link.readyState == "complete") {
-                        link.onreadystatechange = null;
-                        callbackCss();
-                    }
-                }
-                link.onerror = function () {
-                    callbackCss();
-                }
-            }
-            else {
-                link.onload = callbackCss;
-                link.onerror = callbackCss;
-            }
             link.type = "text/css";
             link.rel = "stylesheet";
             link.href = window.delayCss[i];
-            document.getElementsByTagName("head")[0].appendChild(link);
+            df.appendChild(link);
         }
+        document.getElementsByTagName('head')[0].appendChild(df);
     }
 
 
@@ -52,7 +38,7 @@ function delayedLoading() {
         }
     }
     function callbackJs(load) {
-        console.log(load.type + ': ' + load.target.src);
+        console.log(load.type + 'js: ' + load.target.src);
         loadedCount++;
         if (loadedCount == window.delayJs.length) {
             // все скрипты загружены, переходим к выполнению JS
